afd4ab914c27b6908c4b7afad4861db0bc9a00c8
Peter Hurford <peter@peterhurford.com>
2014-05-09 01:33:32 -0400

    Finished answer for exercise 1 in data structures

--- data_structures/exercise1.r
+++ data_structures/exercise1.r
@@ -1,3 +1,7 @@
-# What are the six types of atomic vectors? How does a list differ from an atomic vector?
-
+### What are the six types of atomic vectors?
 # character, integer, double, logical, raw, complex
+
+### How does a list differ from an atomic vector?
+# Both are 1-dimensional data structures, but an atomic vector must only contain elements of the same type
+# whereas a list can contain elements of different types (even other lists).  This means lists can be recursive
+# (containing themselves), whereas atomic vectors are not (atomic vectors cannot contain atomic vectors).
