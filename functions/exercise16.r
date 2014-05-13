e4237975251f0c0cc80c90c1acd7a1da61cfc186
Ryland Ely <rylandely@Avants-MacBook-Pro-2.local>
2014-05-13 15:49:58 -0500

    a few env exercises

--- functions/exercise16.r
+++ functions/exercise16.r
@@ -13,3 +13,5 @@
 ### capture.output2(cat("a", "b", "c", sep = "\n"))
 ### #> [1] "a" "b" "c"
 ### You might want to compare this function to the real capture.output() and think about the simplifications I've made. Is the code easier to understand or harder? Have I removed important functionality?
+
+### Compare capture.output() to capture.output2(). How do the functions differ? What features have I removed to make the key ideas easier to see? How have I rewritten the key ideas to be easier to understand?
