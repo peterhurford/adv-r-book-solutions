6e8c34c61691607b5c9b11fcbcd8a38e93df0af9
Peter Hurford <peter@peterhurford.com>
2014-09-03 09:57:41 -0500

    Added exercise 14 to functions

--- functions/exercise14.r
+++ functions/exercise14.r
@@ -1,1 +1,3 @@
 ### How does the chdir parameter of source() compare to in_dir()? Why might you prefer one approach to the other?
+
+# The R-Doc for `source()` (use `help(source)`) shows that chdir indicates that "the R working directory is temporarily changed to the directory containining file" if TRUE, that is whether or not your file is not in the current working directory.  On the other hand, `in_dir` (`help(indir)`) can save the previous working directory whenever a new working directory is set, so that it can still be accessed.
