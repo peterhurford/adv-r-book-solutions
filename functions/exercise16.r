7358828bfa7e372b673531abc7a1ea2764eed0cd
Peter Hurford <peter@peterhurford.com>
2014-09-02 19:14:12 -0500

    Uncommented our R code in exercise16.r

--- functions/exercise16.r
+++ functions/exercise16.r
@@ -1,16 +1,18 @@
 ### We can use on.exit() to implement a simple version of capture.output().
-### 
-### capture.output2 <- function(code) {
-###   temp <- tempfile()
-###   on.exit(file.remove(temp), add = TRUE)
-### 
-###   sink(temp)
-###   on.exit(sink(), add = TRUE)
-### 
-###   force(code)
-###   readLines(temp)
-### }
-### capture.output2(cat("a", "b", "c", sep = "\n"))
+
+capture.output2 <- function(code) {
+   temp <- tempfile()
+   on.exit(file.remove(temp), add = TRUE)
+ 
+   sink(temp)
+   on.exit(sink(), add = TRUE)
+ 
+   force(code)
+   readLines(temp)
+}
+
+capture.output2(cat("a", "b", "c", sep = "\n"))
+
 ### #> [1] "a" "b" "c"
 ### You might want to compare this function to the real capture.output() and think about the simplifications I've made. Is the code easier to understand or harder? Have I removed important functionality?
 
