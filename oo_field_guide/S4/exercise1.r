3621fec0903fb42d3559d3b05825c9ad21879d92
Peter Hurford <peter@peterhurford.com>
2015-02-16 19:52:51 -0500

    enforce style guide on oo/S4/1

--- oo_field_guide/S4/exercise1.r
+++ oo_field_guide/S4/exercise1.r
@@ -1,25 +1,29 @@
 ### Which S4 generic has the most methods defined for it? Which S4 class has the most methods associated with it?
 
-all.generics <- as.character(getGenerics())
-all.classes <- as.character(getClasses())
-get.num.methods <- function(fname, mode) {
-  if (mode == 'generic') f <- getGeneric(fname)
-  else if (mode == 'class') f <- fname
-  else return(0)
-  if (is.null(f)) return(0)
-  if (mode == 'generic') n <- capture.output(showMethods(f))
-  else n <- capture.output(showMethods(class=f))
-  length(n)-2
-}
-get.num.methods.generic <- function(fname) { get.num.methods(fname, 'generic') }
-get.num.methods.class <- function(fname) { get.num.methods(fname, 'class') }
+all_generics <- as.character(getGenerics())
+all_classes <- as.character(getClasses())
 
-number_methods <- sapply(all.generics,get.num.methods.generic)
+get_num_methods <- function(fname, mode) {
+  if (identical(mode, 'generic')) f <- getGeneric(fname)
+  else if (identical(mode, 'class')) f <- fname
+  else return(0)
+
+  if (is.null(f)) return(0)
+  n <- if (identical(mode, 'generic') capture.output(showMethods(f))
+  else capture.output(showMethods(class = f))
+
+  length(n) - 2
+}
+
+get_num_methods.generic <- function(fname) { get_num_methods(fname, 'generic') }
+get_num_methods.class <- function(fname) { get_num_methods(fname, 'class') }
+
+number_methods <- sapply(all_generics, get_num_methods.generic)
 which.max(number_methods)
 # coerce 
 #     66
 
-number_class_methods <- sapply(all.classes,get.num.methods.class)
+number_class_methods <- sapply(all_classes, get_num_methods.class)
 which.max(number_class_methods)
 # ANY
 #  13
