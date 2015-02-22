b74a58612d0aefca1b0a5d94b7b0715ee985c0da
Peter Hurford <peter@peterhurford.com>
2015-02-21 23:08:19 -0600

    mark environments/bindings/1 and 2 to TODO

---
+++ environments/bindings/exercise2.r
@@ -0,0 +1,3 @@
+### Create a version of assign() that will only bind new names, never re-bind old names. Some programming languages only do this, and are known as single assignment languages.
+
+#TODO:
---
+++ environments/bindings/exercise3.r
@@ -0,0 +1,3 @@
+### Write an assignment function that can do active, delayed, and locked bindings. What might you call it? What arguments should it take? Can you guess which sort of assignment it should do based on the input?
+
+#TODO:
