23831b4fae7a6b918c358a0fdae76c76f12634b4
Peter Hurford <peter@peterhurford.com>
2015-02-21 21:07:45 -0600

    Todo for oo/RC/2

---
+++ oo_field_guide/exercise2.r
@@ -0,0 +1,3 @@
+### I claimed that there aren’t any RC classes in base R, but that was a bit of a simplification. Use getClasses() and find which classes extend() from envRefClass. What are the classes used for? (Hint: recall how to look up the documentation for a class.)
+
+#TODO:
