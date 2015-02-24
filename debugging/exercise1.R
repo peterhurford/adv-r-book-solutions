0fa56e2079c6dcf48347072429a265bfcc87c270
Peter Hurford <peter@peterhurford.com>
2015-02-24 16:25:00 -0600

    debugging/1

--- debugging/exercise1.R
+++ debugging/exercise1.R
@@ -3,12 +3,27 @@
 message2error <- function(code) {
   withCallingHandlers(code, message = function(e) stop(e))
 }
-message2error <- function(code) {
+message2error_ <- function(code) {
   tryCatch(code, message = function(e) stop(e))
 }
 
-#TODO:
-
 ### What is the main advantage of withCallingHandlers() in this scenario? (Hint: look carefully at the traceback.)
 
-#TODO:
+rm(a)
+message2error(a)
+#Error in withCallingHandlers(code, message = function(e) stop(e)) :
+#  object 'a' not found
+traceback()
+#2: withCallingHandlers(code, message = function(e) stop(e)) at #2
+#1: message2error(a)
+message2error_(a)
+#Error in doTryCatch(return(expr), name, parentenv, handler) :
+#  object 'a' not found
+traceback()
+#5: doTryCatch(return(expr), name, parentenv, handler)
+#4: tryCatchOne(expr, names, parentenv, handlers[[1L]])
+#3: tryCatchList(expr, classes, parentenv, handlers)
+#2: tryCatch(code, message = function(e) stop(e)) at #2
+#1: message2error_(a)
+
+# The traceback for the tryCatch implementation (message2error_) goes deeper than we would like, past tryCatch, which confuses the user.
