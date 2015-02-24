f0ba416196a2271a21e8c102b587a4695d292d10
Peter Hurford <peter@peterhurford.com>
2015-02-24 16:55:46 -0600

    debugging/3

--- debugging/exercise3.R
+++ debugging/exercise3.R
@@ -5,4 +5,29 @@ lag <- function(x, n = 1L) {
   c(rep(NA, n), x[seq_len(xlen - n)])
 }
 
-#TODO:
+lag_ <- function(x, n = 1L) {
+  if (!is.numeric(n) | length(n) != 1) stop('n must be a numeric length-1 vector.')
+  xlen <- length(x)
+  if (n > xlen) stop('n must be less than the length of x.')
+  if (n == 0) warning('n is 0, so the lagged vector and the unlagged vector will be identical.')
+  c(rep(NA, n), x[seq_len(xlen - n)])
+}
+
+lag(seq(4), c(1, 2))
+# Error in rep(NA, n) : invalid 'times' argument
+lag_(seq(4), c(1, 2))
+# Error in lag_(seq(4), c(1, 2)) : n must be a numeric length-1 vector.
+
+lag(seq(4), 0)
+# [1] 1 2 3 4
+lag_(seq(4), 0)
+# [1] 1 2 3 4
+# Warning message:
+# In lag_(seq(4), 0) :
+#  n is 0, so the lagged vector and the unlagged vector will be identical.
+
+lag(seq(4), 5)
+# Error in seq_len(xlen - n) :
+#  argument must be coercible to non-negative integer
+lag_(seq(4), 5)
+# Error in lag_(seq(4), 5) : n must be less than the length of x.
