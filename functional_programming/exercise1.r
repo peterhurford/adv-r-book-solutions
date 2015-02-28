83a82f10d99ff62a661b4f7064c3a134e8cf9b9b
Peter Hurford <peter@peterhurford.com>
2015-02-27 23:43:25 -0600

    functional_programming/1

---
+++ functional_programming/exercise1.r
@@ -0,0 +1,7 @@
+### Given a function, like "mean", match.fun() lets you find a function. Given a function, can you find its name?
+
+# No.
+
+### Why doesn’t that make sense in R?
+
+# Because functions are anonymous in R, they are not bound to any particular name.  Any function could be given any name.
