118279791c4b3695f17520da96e4fea61a53450d
Peter Hurford <peter@peterhurford.com>
2015-02-28 21:31:05 -0600

    functionals/friends/4

---
+++ functionals/friends/exercise4.r
@@ -0,0 +1,11 @@
+### What does replicate() do?
+
+# Replicate runs a certain expression multiple times.
+
+### What sort of for loop does it eliminate?
+
+# It eliminates a loop over the number of times you want to run the expression.
+
+### Why do its arguments differ from lapply() and friends?
+
+# Because it isn't applying an expression to a number of inputs, but rather running an expression a number of times.
