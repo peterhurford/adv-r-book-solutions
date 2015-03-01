333166830635b65970fe3b9bfdf9ae93d5a3bd10
Peter Hurford <peter@peterhurford.com>
2015-02-28 18:21:33 -0600

    functionals/3

---
+++ functionals/exercise3.r
@@ -0,0 +1,19 @@
+### Use both for loops and lapply() to fit linear models to the mtcars using the formulas stored in this list:
+
+formulas <- list(
+  mpg ~ disp,
+  mpg ~ I(1 / disp),
+  mpg ~ disp + wt,
+  mpg ~ I(1 / disp) + wt
+)
+
+# For loop
+fits <- list()
+i <- 1
+for (formula in formulas) { fits[[i]] <- lm(formula, data = mtcars); i <- i + 1 }
+fits
+
+# lapply
+rm(fits)
+fits <- lapply(formulas, lm, data = mtcars)  # or `lapply(formulas, function(f), lm(f, data = mtcars))`
+fits
