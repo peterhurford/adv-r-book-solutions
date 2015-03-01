d3bdff4ea7b154273ba7fbde5ddeabc18c885672
Peter Hurford <peter@peterhurford.com>
2015-02-28 21:27:43 -0600

    functionals/friends/3

---
+++ functionals/friends/exercise3.r
@@ -0,0 +1,8 @@
+### The following code simulates the performance of a t-test for non-normal data. Use sapply() and an anonymous function to extract the p-value from every trial.
+
+trials <- replicate(
+  100, 
+  t.test(rpois(10, 10), rpois(7, 10)),
+  simplify = FALSE
+)
+sapply(trials, function(x) x$p.value)
