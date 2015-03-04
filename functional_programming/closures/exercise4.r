fdc8fddaa6110d56507f7feed34637302a82a3c3
Peter Hurford <peter@peterhurford.com>
2015-03-03 18:56:16 -0600

    functional_programming/closures/5 is a todo

---
+++ functional_programming/closures/exercise4.r
@@ -0,0 +1,4 @@
+### What does ecdf() do? What does it return?
+
+# ecdf() rewrites the arguments passed to it, and then calls approxfun() with those variables. 
+#TODO: sys.call nuances
---
+++ functional_programming/closures/exercise5.r
@@ -0,0 +1,12 @@
+### Create a function that creates functions that compute the ith central moment <http://en.wikipedia.org/wiki/Central_moment> of a numeric vector. You can test it by running the following code.
+
+moment <- function(vec, i) {
+#TODO: Hadley expects too much math.
+}
+
+m1 <- moment(1)
+m2 <- moment(2)
+
+x <- runif(100)
+stopifnot(all.equal(m1(x), 0))
+stopifnot(all.equal(m2(x), var(x) * 99 / 100))
