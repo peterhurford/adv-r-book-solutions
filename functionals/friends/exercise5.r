cf93e8e4bef26bc8d1e9300aa58ea7f736e342ef
Peter Hurford <peter@peterhurford.com>
2015-03-03 19:14:39 -0600

    functionals/friends/5

--- functionals/friends/exercise5.r
+++ functionals/friends/exercise5.r
@@ -1,12 +1,10 @@
 ### Implement a version of lapply() that supplies FUN with both the name and the value of each component.
 
 lapply2 <- function(X, FUN, ...) {
-    out <- vector("list", length(x))
-    for (i in seq_along(x)) {
-      out[[i]] <- f(x[[i]], ...)
+    out <- vector("list", length(X))
+    for (i in seq_along(X)) {
+      out[[i]] <- FUN(c(names(X)[[i]] = X[[i]]), ...)
     }
     out
   }
 }
-
-#TODO:
