be1bfce3f350b9d33f80da312f600021b5ea8001
Peter Hurford <peter@peterhurford.com>
2015-03-03 19:12:03 -0600

    I don't want to do the integration exercises

---
+++ functional_programming/integration/exercise2.r
@@ -0,0 +1,3 @@
+### The trade-off between integration rules is that more complex rules are slower to compute, but need fewer pieces. For sin() in the range [0, π], determine the number of pieces needed so that each rule will be equally accurate. Illustrate your results with a graph. How do they change for different functions? sin(1 / x^2) is particularly challenging.
+
+#TODO:
