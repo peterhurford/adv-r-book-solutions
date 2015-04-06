84d5c1524e29178b2b52f92448eb8dc2c564dbb5
Igor Veksler <iveksl2@gmail.com>
2015-04-05 22:38:34 -0500

    Update exercise4.r

--- functional_programming/closures/exercise4.r
+++ functional_programming/closures/exercise4.r
@@ -4,5 +4,5 @@
 #TODO: sys.call nuances
 
 ## Igor 
-Compute an empirical cumulative distribution function. The e.c.d.f. (empirical cumulative distribution function) Fn is a step function with jumps i/n at observation values, where i is the number of tied observations at that value. 
+4. Compute an empirical cumulative distribution function. The e.c.d.f. (empirical cumulative distribution function) Fn is a step function with jumps i/n at observation values, where i is the number of tied observations at that value. 
 Missing values are ignored.
