7a1864e5ab6b12f5cab30fd839386a92ade147f5
Peter Hurford <peter@peterhurford.com>
2015-04-13 12:37:41 -0500

    Clarify that lists are vectors

--- data_structures/lists/4.R
+++ data_structures/lists/4.R
@@ -1,5 +1,8 @@
 ### Why do you need to use unlist() to convert a list to an atomic vector? Why doesn’t as.vector() work?
 
-# `as.vector` can be used to convert a non-recursive list to an atomic vector
-# if the desired `mode` is passed explicitly.  However, `as.vector` cannot handle
-# recursion within lists.  Only `unlist` can do that.
+# Recall that lists are considered vectors, so `as.vector` will leave it the same.
+
+# However, `as.vector` can be used to convert a non-recursive list to an atomic vector
+# if the desired `mode` is passed explicitly.
+
+# But the core problem is that `as.vector` cannot handle recursion within lists.  Only `unlist` can do that.
