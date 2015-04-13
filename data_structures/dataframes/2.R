21fa1d52c464257a14addccaf876f8d42a5b15ee
Peter Hurford <peter@peterhurford.com>
2015-04-13 11:36:48 -0500

    We expect coercion, not recursion

--- data_structures/dataframes/2.R
+++ data_structures/dataframes/2.R
@@ -1,6 +1,6 @@
 ### What does as.matrix() do when applied to a data frame with columns of different types?
 
-# Recall that matrices are atomic (cannot have different types).  Therefore we would expect recursion.
+# Recall that matrices are atomic (cannot have different types).  Therefore we would expect type coercion.
 
 > as.matrix(data.frame(a = 1, b = 'char'))
      a   b
