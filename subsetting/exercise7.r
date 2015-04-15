ba51eac2a753cdf07782987034ab1ffc6dc2f1a1
Peter Hurford <peter@peterhurford.com>
2015-04-15 13:40:08 -0500

    better residual degrees of freedom

--- subsetting/exercise7.r
+++ subsetting/exercise7.r
@@ -1,12 +1,14 @@
-### Given a linear model, e.g. mod <- lm(mpg ~ wt, data = mtcars), extract the residual degrees of freedom. Extract the R squared from the model summary (summary(mod)).
+### Given a linear model, e.g. mod <- lm(mpg ~ wt, data = mtcars), extract the residual degrees of freedom. 
 
 mod <- lm(mpg ~ wt, data = mtcars)
-x <- summary(mod)
+mod_sum <- summary(mod)
 
-# residual degrees of freedom
-  x$df[2]
+mod_sum$df[2]
 # [1] 30
 
-# R-squared
-  x$r.squared
+mod$df.residual
+# [1] 30
+
+### Extract the R squared from the model summary (summary(mod)).
+mod_sum$r.squared
 # [1] 0.7528328 
