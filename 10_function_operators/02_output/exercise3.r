ec9ab07443da2aee5db0d58da7a74761dade2f78
iveksl2 <igor.veksler@avantcredit.com>
2015-04-22 21:40:01 -0500

    more function operators

--- 10_function_operators/02_output/exercise3.r
+++ 10_function_operators/02_output/exercise3.r
@@ -14,5 +14,16 @@ file.create('tmp')
 dirs_changed(file.remove)('tmp')
 # [1] "tmp"
 
+## Another function
+file_deltas <- function(directory) {
+  orig_files <- dir(directory)
+  function(...) {
+    created <- setdiff(dir(directory), orig_files)
+    deleted <- setdiff(orig_files, dir(directory))
+    cat('created = ', created, 'deleted = ', deleted, '\n' )
+  }
+}
+
 ### What other global effects of functions might you want to track?
 # Any side effect seems fair game, like printing, database interactions, adding or removing global variables, etc.
+# Can also track meta-data related to files such as last modification time, etc. 
