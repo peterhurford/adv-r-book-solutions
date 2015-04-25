ad2a9295c81d22e094d6081b9712ac3a3de1a7f4
Peter Hurford <peter@peterhurford.com>
2015-04-25 14:07:18 -0500

    Wrap

--- 08_functional_programming/02_closures/exercise3.r
+++ 08_functional_programming/02_closures/exercise3.r
@@ -1,3 +1,4 @@
 ### What does approxfun() do? What does it return?
 
-# Approxfun rewrites arguments passed to it, and then returns a function that calls another function, .approxfun, with those rewritten variables.
+# Approxfun rewrites arguments passed to it, and then returns a function that calls another function,
+# .approxfun, with those rewritten variables.
