f3249babe6ee947c7f09fff1e771eda3463a7fb2
iveksl2 <iveksl2@gmail.com>
2015-04-28 23:20:02 -0500

    modify where to return all environments for a binding, works but returns null also

--- 06_environments/02_recursion/exercise1.r
+++ 06_environments/02_recursion/exercise1.r
@@ -1,3 +1,14 @@
 ### Modify where() to find all environments that contain a binding for name.
-
-# TODO: What is "a binding for name"
+where2 <- function(name, env = parent.frame()) {
+  if (identical(env, emptyenv())) {
+    # Base case
+    return()
+  } else if (exists(name, envir = env, inherits = FALSE)) {
+    # Success case
+    str(env)
+    where2(name, parent.env(env))
+  } else {
+    # Recursive case
+    where2(name, parent.env(env))
+  }
+}
