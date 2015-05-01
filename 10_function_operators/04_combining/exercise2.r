6506d6860372ac6c6d524055565202d7b2247c00
Peter Hurford <peter@peterhurford.com>
2015-05-01 12:46:35 -0500

    function_operators/combining/2

--- 10_function_operators/04_combining/exercise2.r
+++ 10_function_operators/04_combining/exercise2.r
@@ -6,7 +6,7 @@ and2 <- function(...) {
   fns <- lapply(list(...), force)
   function(...) {
     Reduce(
-      and,
+      `&&`,
       lapply(
         fns,
         function(fn) { do.call(fn, list(...)) }
@@ -14,4 +14,25 @@ and2 <- function(...) {
     )
   }
 }
-#TODO: Finish
+
+lazy_and2 <- function(...) {
+  fns <- lapply(list(...), force)
+  function(...) {
+    for (fn in fns) {
+      if (!isTRUE(do.call(fn, list(...)))) { return(FALSE); break }
+    }
+    TRUE
+  }
+}
+
+and2(is.numeric, function(x) x > 4.0)(10)   # TRUE
+and2(is.numeric, function(x) x > 4.0)(3)    # FALSE
+and2(is.numeric, function(x) x > 4.0)("10") # FALSE
+lazy_and2(is.numeric, function(x) { Sys.sleep(2); x > 4.0 })("10") # FALSE (but executes the sleep because it isn't lazy)
+
+lazy_and2(is.numeric, function(x) x > 4.0)(10)   # TRUE
+lazy_and2(is.numeric, function(x) x > 4.0)(3)    # FALSE
+lazy_and2(is.numeric, function(x) x > 4.0)("10") # FALSE
+lazy_and2(is.numeric, function(x) { Sys.sleep(2); x > 4.0 })("10") # FALSE (and doesn't evaluate the sleep)
+
+#TODO: Can you get both the Reduce AND the laziness?
