f03b58712610b9e6b7df819134609e9836cfd369
Peter Hurford <peter@peterhurford.com>
2015-05-03 18:48:30 -0400

    nonstandard/capturing/2

---
+++ 11_nonstandard_eval/01_capturing/exercise2.r
@@ -0,0 +1,8 @@
+### Why does as.Date.default() use substitute() and deparse()?
+
+# To print the input into as.Date in the error message to show the
+# user what they typed.
+
+### Why does pairwise.t.test() use them?
+
+# To populate the name of the returned list.
