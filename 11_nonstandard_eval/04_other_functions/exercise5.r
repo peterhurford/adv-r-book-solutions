0d8ba35f4ad59613f9429431ee1353deca4f6812
Peter Hurford <peter@peterhurford.com>
2015-05-11 11:57:43 -0500

    finish nonstandard/other/5

--- 11_nonstandard_eval/04_other_functions/exercise5.r
+++ 11_nonstandard_eval/04_other_functions/exercise5.r
@@ -13,3 +13,17 @@ subset2_q <- function(x, cond, env = parent.frame()) {
 # The modified version of subset2_q allows you to specify an env in which to
 # evaluate the condition, which allows you to run subset2_q in more situations
 # (such as within a dataframe).
+
+
+### Rewrite subset2() and subscramble() to use this improved version.
+
+subset2 <- function(x, condition, env = parent.frame()) {
+  subset2_q(x, substitute(condition), env)
+}
+
+scramble <- function(x) x[sample(nrow(x)), ]
+
+subscramble <- function(x, condition, env = parent.frame()) {
+  condition <- substitute(condition, env)
+  scramble(subset2_q(x, condition, env))
+}
