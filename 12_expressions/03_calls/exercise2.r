f17f5ea9d6ae6b29f82a7860da027853bbd77a97
Peter Hurford <peter@peterhurford.com>
2015-05-17 18:58:54 -0400

    expressions/calls/2

---
+++ 12_expressions/03_calls/exercise2.r
@@ -0,0 +1,14 @@
+### Implement a pure R version of do.call().
+
+library(magrittr)
+do.call2 <- function(what, args) {
+  if (!is.list(args)) { args <- list(args) }
+  substitute(what) %>% append(args, ., 0) %>% 
+    as.call(.) %>% eval(.)
+}
+
+do.call2(median, 1:10)
+# [1] 5.5
+
+do.call2(paste, list("hi", "hello"))
+# [1] "hi hello"
