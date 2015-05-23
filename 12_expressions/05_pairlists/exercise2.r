68bdbe1eb1dbfaa4a6f09f1ada5c610e59b21cd3
Peter Hurford <peter@peterhurford.com>
2015-05-23 10:37:33 -0500

    expressions/pairlists/2

---
+++ 12_expressions/05_pairlists/exercise2.r
@@ -0,0 +1,12 @@
+### Read the documentation and source code for pryr::partial(). What does it do?
+### How does it work?
+
+# It takes a function and arguments and then constructs a call of that
+# function with those args.
+
+
+### Read the documentation and source code for pryr::unenclose(). What does it
+### do and how does it work?
+
+# It takes a closure and substitutes the variables in that closure for its values
+# found in its environment, which results in the explicit function.
