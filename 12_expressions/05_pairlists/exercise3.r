6c58948d6bef8934709eef4049f752ef57bb24a3
Peter Hurford <peter@peterhurford.com>
2015-05-26 14:18:03 -0500

    expressions/parsing/6

--- 12_expressions/05_pairlists/exercise3.r
+++ 12_expressions/05_pairlists/exercise3.r
@@ -2,10 +2,12 @@
 
 curve3 <- function(expr, xlim = c(0, 1), n = 100,
                    env = parent.frame()) {
+  # Define axes with env!
   env2 <- new.env(parent = env)
   env2$x <- seq(xlim[1], xlim[2], length = n)
-
   y <- eval(substitute(expr), env2)
+
+  # Plot!
   plot(env2$x, y, type = "l", 
     ylab = deparse(substitute(expr)))
 }
@@ -17,9 +19,11 @@ curve2 <- function(expr, xlim = c(0, 1), n = 100,
                    env = parent.frame()) {
   f <- pryr::make_function(alist(x = ), substitute(expr), env)
 
+  # Define axes!
   x <- seq(xlim[1], xlim[2], length = n)
   y <- f(x)
 
+  # Plot!
   plot(x, y, type = "l", ylab = deparse(substitute(expr)))
 }
 
