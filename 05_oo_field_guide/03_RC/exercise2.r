384ef20ef1b74371444f78d6d0a70b9cd98a1301
iveksl2 <iveksl2@gmail.com>
2015-05-28 22:18:20 -0500

    not sure how to do this, added a hint

--- 05_oo_field_guide/03_RC/exercise2.r
+++ 05_oo_field_guide/03_RC/exercise2.r
@@ -1,3 +1,4 @@
 ### I claimed that there aren’t any RC classes in base R, but that was a bit of a simplification. Use getClasses() and find which classes extend() from envRefClass. What are the classes used for? (Hint: recall how to look up the documentation for a class.)
 
+# You can find the documentation for a class with a special syntax: class?className, e.g., class?mle.
 #TODO:
