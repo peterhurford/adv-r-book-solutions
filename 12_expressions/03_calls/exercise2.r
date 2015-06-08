b5bd0def248cc7483653aaed56c6aaa7a6eaf493
iveksl2 <iveksl2@gmail.com>
2015-06-07 21:34:12 -0500

    alternative

--- 12_expressions/03_calls/exercise2.r
+++ 12_expressions/03_calls/exercise2.r
@@ -3,7 +3,7 @@
 library(magrittr)
 do.call2 <- function(what, args) {
   if (!is.list(args)) { args <- list(args) }
-  substitute(what) %>% append(args, ., 0) %>% 
+  substitute(what) %>% append(args, ., 0) %>%
     as.call(.) %>% eval(.)
 }
 
@@ -12,3 +12,8 @@ do.call2(median, 1:10)
 
 do.call2(paste, list("hi", "hello"))
 # [1] "hi hello"
+
+## alternative
+do2.call <- function(fun, ...) {
+  eval(call(as.character(substitute(fun)), unlist(...)))
+}
