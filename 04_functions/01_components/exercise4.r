e1b3a63c0539de871728b522604110c0aa18c7d1
Bohdan Khomtchouk <khomtchoukmed@gmail.com>
2015-12-05 21:54:02 -0500

    Update exercise4.r

--- 04_functions/01_components/exercise4.r
+++ 04_functions/01_components/exercise4.r
@@ -1,3 +1,3 @@
 ### When does printing a function not show what environment it was created in?
 
-# When the function is a primitive.
+# When the function is a primitive, or when the function is created in the global environment.
