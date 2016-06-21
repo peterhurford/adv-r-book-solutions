3a196caa279c39bea264f561f2fa8d71bb369e1f
samuel-liyi <liyisepku@gmail.com>
2016-06-21 17:22:38 +0800

    update expression/calls/ex1

--- 13_expressions/03_calls/exercise1.r
+++ 13_expressions/03_calls/exercise1.r
@@ -12,27 +12,20 @@ identical(a, b)
 # `a` is calling function `mean` with the arg `1:10`,
 # whereas `b` is calling `mean` with the arg `quote(1:10)`.
 # The second is passing the second argument as an unevaluated expression rather,
-# these have the same result because of lazy evaluation.
-
+# when the arg is not a constant,the result would be different
+x <- seq(10)
+(a <- call("mean", x))
+#> mean(1:10)
+(b <- call("mean", quote(x)))
+#> mean(x)
+x <- seq(20)
+eval(a)
+#> [1] 5.5
+eval(b)
+#> [2] 10.5
 
 ### Which one should you prefer?
 
-# However I personally
-# prefer the first methodology at is it more explicit and more robust against
-# early evaluation.
-# Both `a` and `b` work to get the intended result...
-
-eval(a)
-# [1] 5.5
-
-eval(b)
-# [1] 5.5
-
-# ...but `eval(b)` is a little (not noticably) slower and is more verbose.
-microbenchmark::microbenchmark(eval(a), eval(b))
-# Unit: microseconds
-#     expr   min     lq median     uq    max neval
-#  eval(a) 4.438 4.6250 4.7950 4.9345 10.551   100
-#   eval(b) 4.663 4.8265 4.9655 5.1945 37.992   100
-
-# so `a` should be prefered.
+# The quote version is prefered, it's better to have call args depends on the
+# calling environment rather than the enclosing environment,that's more similar
+# to normal function behavior.
