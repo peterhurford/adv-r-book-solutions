b73bf18abdafaf05f6b723140aa4398d5cfdabc3
Jorge Cimentada <cimentadaj@gmail.com>
2016-09-15 13:01:34 +0200

    Function was not working correctly
    Function was missing some parenthesis. And the result in line 21 appears to be wrong.

--- 05_oo_field_guide/02_S4/exercise1.r
+++ 05_oo_field_guide/02_S4/exercise1.r
@@ -1,18 +1,15 @@
 ### Which S4 generic has the most methods defined for it? Which S4 class has the most methods associated with it?
 
-all_generics <- as.character(getGenerics())
-all_classes <- as.character(getClasses())
-
 get_num_methods <- function(fname, mode) {
   if (identical(mode, 'generic')) f <- getGeneric(fname)
   else if (identical(mode, 'class')) f <- fname
   else return(0)
-
+  
   if (is.null(f)) return(0)
-  n <- if (identical(mode, 'generic') capture.output(showMethods(f))
-  else capture.output(showMethods(class = f))
-
-  length(n) - 2
+  if (identical(mode, 'generic')) n <- capture.output(showMethods(f))
+           else n <- capture.output(showMethods(class = f))
+           
+           length(n) - 2
 }
 
 get_num_methods.generic <- function(fname) { get_num_methods(fname, 'generic') }
