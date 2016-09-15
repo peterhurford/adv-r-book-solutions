e975addadbc59f6adfff45673a6e507a8a61c942
Jorge Cimentada <cimentadaj@gmail.com>
2016-09-15 13:04:17 +0200

    Simplification of the function
    I eliminated a lot of extra steps and still get the same result. Note that in line 18 and 23 I subset the maximum instead of showing the index, as you were doing before.

--- 05_oo_field_guide/02_S4/exercise1.r
+++ 05_oo_field_guide/02_S4/exercise1.r
@@ -1,26 +1,25 @@
 ### Which S4 generic has the most methods defined for it? Which S4 class has the most methods associated with it?
 
+all_generics <- as.character(getGenerics())
+all_classes <- as.character(getClasses())
+
 get_num_methods <- function(fname, mode) {
-  if (identical(mode, 'generic')) f <- getGeneric(fname)
-  else if (identical(mode, 'class')) f <- fname
-  else return(0)
-  
-  if (is.null(f)) return(0)
-  if (identical(mode, 'generic')) n <- capture.output(showMethods(f))
-           else n <- capture.output(showMethods(class = f))
-           
-           length(n) - 2
+  if (identical(mode, 'generic')) {
+    n <- capture.output(showMethods(fname))
+  } else if (identical(mode, 'class')) {
+    n <- capture.output(showMethods(class = fname))
+  } else {
+    return(0)
+  }
+  length(n) - 2
 }
 
-get_num_methods.generic <- function(fname) { get_num_methods(fname, 'generic') }
-get_num_methods.class <- function(fname) { get_num_methods(fname, 'class') }
+number_methods <- sapply(all_generics, get_num_methods, 'generic')
+number_methods[which.max(number_methods)]
+# initialize 
+#  47 
 
-number_methods <- sapply(all_generics, get_num_methods.generic)
-which.max(number_methods)
-# coerce 
-#     66
-
-number_class_methods <- sapply(all_classes, get_num_methods.class)
-which.max(number_class_methods)
-# ANY
-#  13
+number_class_methods <- sapply(all_classes, get_num_methods, 'class')
+number_class_methods[which.max(number_class_methods)]
+# ANY 
+# 160 
